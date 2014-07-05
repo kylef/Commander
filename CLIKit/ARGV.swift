6707ca714027d5190e2bf25483c69fc2c4cc0afc
Radosław Pietruszewski <radex@Radosaws-MacBook-Pro.local>
2014-07-05 16:44:59 +0200

    ARGV: basic implementation + tests

---
+++ CLIKit/ARGV.swift
@@ -0,0 +1,78 @@
+//
+//  ARGV.swift
+//  CLIKit
+//
+//  Created by Radoslaw Pietruszewski on 05/07/14.
+//  Copyright (c) 2014 Cocode. All rights reserved.
+//
+
+class ARGV {
+    
+    enum ParameterType {
+        case Argument, Option, Flag
+    }
+    
+    let originalArgs: String[]
+    var arguments = String[]()
+    var options = Dictionary<String, String>()
+    var flags = Dictionary<String, Bool>()
+    
+    init(_ args: String) {
+        originalArgs = args.componentsSeparatedByString(" ")
+        
+        for arg in originalArgs {
+            switch _parameterType(arg) {
+            case .Argument:
+                arguments.append(arg)
+            case .Option:
+                let (key, value) = _optionParameter(arg)
+                options[key] = value
+            case .Flag:
+                let (key, value) = _flagParameter(arg)
+                flags[key] = value
+            }
+        }
+    }
+    
+    func shift() -> String? {
+        if arguments.count > 0 {
+            return arguments.removeAtIndex(0)
+        } else {
+            return nil
+        }
+    }
+    
+    func option(name: String) -> String? {
+        return options.removeValueForKey(name)
+    }
+    
+    func flag(name: String) -> Bool? {
+        return flags.removeValueForKey(name)
+    }
+    
+    func _parameterType(arg: String) -> ParameterType {
+        if startsWith(arg, "--") {
+            if contains(arg, "=") {
+                return .Option
+            } else {
+                return .Flag
+            }
+        } else {
+            return .Argument
+        }
+    }
+    
+    func _optionParameter(arg: String) -> (key: String, value: String) {
+        let components = arg.substringFromIndex(2).componentsSeparatedByString("=") as String[]
+        assert(components.count == 2)
+        return (components[0], components[1])
+    }
+    
+    func _flagParameter(arg: String) -> (key: String, value: Bool) {
+        if startsWith(arg, "--no-") {
+            return (arg.substringFromIndex(5), false)
+        } else {
+            return (arg.substringFromIndex(2), true)
+        }
+    }
+}
