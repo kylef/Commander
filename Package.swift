4c320a3507d621d27f89514eb576664becfee643
Kyle Fuller <kyle@fuller.li>
2016-12-05 20:08:00 +0000

    chore: Use `.git` extension on dependencies URL

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
   name: "Commander",
   dependencies: [
-    .Package(url: "https://github.com/kylef/Spectre", majorVersion: 0, minor: 7),
+    .Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0, minor: 7),
   ]
 )
