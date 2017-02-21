e0cbee1bd73778c1076c675eaf660e97d09f3b32
David Jennes <david.jennes@appwise.be>
2017-02-22 00:03:13 +0100

    Update documentation & examples

--- Examples/hello.swift
+++ Examples/hello.swift
@@ -1,8 +1,8 @@
 import Commander
 
 command(
-  Option("name", "world"),
-  Option("count", 1, description: "The number of times to print.")
+  Option("name", default: "world"),
+  Option("count", default: 1, description: "The number of times to print.")
 ) { name, count in
   for _ in 0..<count {
     print("Hello \(name)")
