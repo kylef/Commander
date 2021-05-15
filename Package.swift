8f55a9d027312f7e609fb65ea6936ff72cf58713
Kyle Fuller <kyle@fuller.li>
2021-05-15 18:24:45 +0100

    chore: use Spectre 0.10.0

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     .library(name: "Commander", targets: ["Commander"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/kylef/Spectre.git", .revision("69da3ff2e59426e16e80aa180eaa1d0772eeee15"))
+    .package(url: "https://github.com/kylef/Spectre.git", from: "0.10.0"),
   ],
   targets: [
     .target(name: "Commander", dependencies: []),
